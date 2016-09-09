c2b2073e026591154083db1e53a0c0a195ee380c
Alexa Herrera <alexa.herrera@digitaloperative.com>
2016-09-08 22:07:34 -0700

    Refactor, add short lists

--- acro.py
+++ acro.py
@@ -8,46 +8,60 @@ import os
 args = sys.argv
 acro = args[1]
 
-combos = {3: ['noun|verb|noun', 'verb|noun|adv', 'noun|verb|adv', 'adv|verb|noun'], 
-          4: ['noun|adv|verb|noun', 'noun|verb|noun|adv', 'adj|noun|adv|verb'],
+combos = {3: ['verb|prep|noun', 'verb|noun|adv', 'noun|verb|adv', 'noun|verb|noun', 'adj|noun|verb'],
+          4: ['noun|adv|verb|noun', 'noun|verb|noun|adv', 'adj|noun|adv|verb',
+              'adj|noun|prep|noun', 'noun|verb|prep|noun'],
           5: ['adj|noun|adv|verb|noun', 'adj|noun|verb|noun|adv']
 }
 
 words = {}
-for type in ['noun', 'verb', 'adv', 'adj']:
-	words[type] = {}
-	for letter in list(string.ascii_lowercase):
-		words[type][letter] = []
 
-	with open(type + '.exc', 'r') as f:
-		reader = csv.reader(f)
-		type_words = list(reader)
-		type_words_flat = [item for sublist in type_words for item in sublist]
-		for word in type_words_flat:
-			first_letter = word[0].lower()
-			words[type][first_letter].append(word)
+def generate_word_dict():
+	for type in ['noun', 'verb', 'adv', 'adj', 'prep']:
+		words[type] = {}
+		for letter in list(string.ascii_lowercase):
+			words[type][letter] = []
 
-length = len(str(acro))
-combo = random.choice(combos[length])
-poses = combo.split('|')
+		with open(type + '.short', 'r') as f:
+			reader = csv.reader(f)
+			type_words = list(reader)
+			type_words_flat = [item for sublist in type_words for item in
+			                   sublist]
+			for word in type_words_flat:
+				first_letter = word[0].lower()
+				words[type][first_letter].append(word)
 
-final_acro = []
-for idx, pos in enumerate(poses):
-	letter = str(acro[idx]).lower()
+def generate_acro(acro):
+	length = len(str(acro))
+	combo = random.choice(combos[length])
+	poses = combo.split('|')
 
-	words_to_choose = []
-	if letter in string.ascii_lowercase:
-		words_to_choose = words[pos][letter]
-	elif letter == '*':
-		letter = random.choice(list(string.ascii_lowercase))
-		words_to_choose = words[pos][letter]
+	final_acro = []
+	for idx, pos in enumerate(poses):
+		letter = str(acro[idx]).lower()
 
-	final_acro.append(random.choice(words_to_choose))
+		words_to_choose = []
+		if letter in string.ascii_lowercase:
+			words_to_choose = words[pos][letter]
+		elif letter == '*':
+			letter = random.choice(list(string.ascii_lowercase))
+			words_to_choose = words[pos][letter]
 
-la_acro = " ".join(final_acro)
+		try:
+			final_acro.append(random.choice(words_to_choose))
+		except IndexError:
+			final_acro.append(letter+'-'+pos)
+			pass
 
-print(la_acro)
+	la_acro = " ".join(final_acro)
+	return la_acro
 
-tts = gTTS(text=la_acro, lang='en')
-tts.save('acro.mp3')
-os.system('cvlc acro.mp3 --play-and-exit')
+def speak(la_acro):
+	tts = gTTS(text=la_acro, lang='en')
+	tts.save('acro.mp3')
+	os.system('cvlc acro.mp3 --play-and-exit 2> /dev/null')
+
+generate_word_dict()
+fa = generate_acro(acro)
+print(fa)
+speak(fa)
